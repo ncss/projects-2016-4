bcb1b7396dde3cbbb6296de3458c8d9e155a81df
willemfs <willemfspeckmann@gmail.com>
2016-01-09 17:18:59 +1100

    Added URLs and handlers
    Amy, Rhiannon and Willem

--- server.py
+++ server.py
@@ -1,9 +1,37 @@
 from tornado.ncss import Server
 
-def index(response):
+def index_handler(response):
     response.write('Hello Team 4: Placebook!')
+
+def signup_handler(response):
+    response.write('Sign Up')
+
+def login_handler(response):
+    response.write('Login')
+
+def search_handler(response):
+    response.write("Search")
+
+def location_handler(response, id):
+    response.write("Location {}".format(id))
+
+def create_handler(response):
+    response.write("Create Location")
+
+def user_handler(response, username):
+    response.write("Profile {}".format(username))
+
+def profile_handler(response):
+    response.write("Profile")
 
 if __name__ == '__main__':
     server = Server()
-    server.register('/', index)
+    server.register('/', index_handler)
+    server.register("/account/signup",signup_handler)
+    server.register("/account/login", login_handler)
+    server.register("/location/search", search_handler)
+    server.register(r"/location/(\d+)", location_handler)
+    server.register("/location/create", create_handler)
+    server.register("/account/profile/([a-z0-9A-Z._]+)", user_handler)
+    server.register("/account/profile", profile_handler)
     server.run()
