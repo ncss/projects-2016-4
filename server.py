f04c2b13009520faa23fa7d6448007c8ab3ab9d8
willemfs <willemfspeckmann@gmail.com>
2016-01-10 15:20:01 +1100

    Amy and Willem: integration with template and design teams

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+from template.render import render
 from tornado.ncss import Server
 
+def get_login(response):
+    return response.get_secure_cookie('username')
 
 def login_check_decorator(fn):
     def inner(response,*args, **kwargs):
@@ -10,38 +13,47 @@ def login_check_decorator(fn):
     return inner
 
 def index_handler(response):
+    logged_in = get_login(response)
     response.write('Hello Team 4: Placebook!')
-    #more login authentication here
 
 def signup_handler(response):
+    logged_in = get_login(response)
+    if logged_in is not None:
+        response.redirect('/')
     response.write('Sign Up')
 
 def login_handler(response):
-    username1 =  response.get_secure_cookie('username')
-    if username1 is not None:
+    logged_in = get_login(response)
+    if logged_in is not None:
         response.redirect("/account/profile")
     else:
-        file = open('test_login_form.html')
-        response.write(file.read())
+        login_page = render("login.html",{})
+        response.write(login_page)
+        # file = open('test_login_form.html')
+        #response.write(file.read())
 
 def search_handler(response):
+    logged_in = get_login(response)
     response.write("Search")
 
 def location_handler(response, id):
+    logged_in = get_login(response)
     response.write("Location {}".format(id))
 
 @login_check_decorator
 def create_handler(response):
+    logged_in = get_login(response)
     response.write("Create Location")
 
 @login_check_decorator
 def user_handler(response, username):
+    logged_in = get_login(response)
     response.write("Profile {}".format(username))
 
 @login_check_decorator
 def profile_handler(response):
-    username1 = response.get_secure_cookie('username')
-    response.write("username: {}".format(str(username1)))
+    logged_in = get_login(response)
+    response.write("username: {}".format(logged_in))
 
 def login_authentication(response):
     username = response.get_field('username')
@@ -56,6 +68,10 @@ def logout_handler(response):
     response.clear_cookie("username")
     response.redirect("/")
 
+@login_check_decorator
+def location_creator(response):
+    pass
+
 if __name__ == '__main__':
     server = Server()
     server.register('/', index_handler)
@@ -63,7 +79,7 @@ if __name__ == '__main__':
     server.register("/account/login", login_handler, post=login_authentication)
     server.register("/location/search", search_handler)
     server.register(r"/location/(\d+)", location_handler)
-    server.register("/location/create", create_handler)
+    server.register("/location/create", create_handler, post=location_creator)
     server.register("/account/profile/([a-z0-9A-Z._]+)", user_handler)
     server.register("/account/profile", profile_handler)
     server.register("/account/logout", logout_handler)
