969364b5241cfae017a0cf77c25dbac1f45f7a55
zanyzebra134 <zoe.bott@yahoo.com.au>
2016-01-10 12:17:10 +1100

    James/Zoe: db: complete user class
    gitignore: include .db*

--- db/db.py
+++ db/db.py
@@ -10,35 +10,65 @@ class DatabaseConnection:
         print(anything)
 
 class User:
-    def __init__(self, conn, id, username, password, email):
-        self.id = id
-        self.conn = conn
+    def __init__(self, conn, username, password, email, id=None):
         self.username = username
         self.password = password
         self.email = email
+        self.id = id
 
+    @staticmethod
     def find(self, username):
-        cur = self.conn.execute('''
-            SELECT id, username, password, email
+        cur = conn.execute('''
+            SELECT username, password, email, id
             FROM users
             WHERE username = ?''', (username,)
         )
+        return User(self.conn, *cur.fetchone())
+
+    @staticmethod
+    def find_all(self):
+        cur = conn.execute('SELECT * FROM users')
+        for row in cur:
+            print(row)
 
     def change_password(self, password):
-        cur = self.conn.execute('''
-            UPDATE users
+        conn.execute('''UPDATE users
             SET password = ?
-            WHERE id = ?''', (password, self.id)
+            WHERE username = ?''', (password, self.username)
         )
         self.password = password
 
     def change_email(self, email):
-        cur = self.conn.execute('''
-            UPDATE users
+        conn.execute('''UPDATE users
             SET email = ?
-            WHERE id = ?''', (email, self.id)
+            WHERE username = ?''', (email, self.username)
         )
         self.email = email
 
+    def change_username(self, username):
+        conn.execute('''
+            UPDATE users
+            SET username = ?
+            WHERE username = ?''', (username, self.username)
+        )
+        self.username = username
+
+    def create(self):
+        conn.execute('''INSERT INTO users (username, password, dp, email, fname, lname)
+            VALUES(?, ?, NULL, ?, NULL, NULL)''',
+            (self.username, self.password, self.email)
+        )
+        cur = conn.execute('''SELECT id FROM users WHERE username = ?''', (self.username,))
+        res = cur.fetchone()
+        self.id = res[0]
+
+
+    def save(self):
+        """cur = self.conn.execute('''UPDATE users
+            SET password = ?, email = ?
+            WHERE username ="""
+
+    def delete(self):
+        cur = self.conn.execute('DELETE FROM users WHERE username = ?', (self.username,))
 
 
