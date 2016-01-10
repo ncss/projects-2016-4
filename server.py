42534a6a720e8412d3cb25f0462d3203b180bbad
willemfs <willemfspeckmann@gmail.com>
2016-01-10 12:31:04 +1100

    Amy, Rhiannon, Willem - added logout, login features

--- server.py
+++ server.py
@@ -11,13 +11,18 @@ def login_check_decorator(fn):
 
 def index_handler(response):
     response.write('Hello Team 4: Placebook!')
+    #more login authentication here
 
 def signup_handler(response):
     response.write('Sign Up')
 
 def login_handler(response):
-    file = open('test_login_form.html')
-    response.write(file.read())
+    username1 =  response.get_secure_cookie('username')
+    if username1 is not None:
+        response.redirect("/account/profile")
+    else:
+        file = open('test_login_form.html')
+        response.write(file.read())
 
 def search_handler(response):
     response.write("Search")
@@ -25,9 +30,11 @@ def search_handler(response):
 def location_handler(response, id):
     response.write("Location {}".format(id))
 
+@login_check_decorator
 def create_handler(response):
     response.write("Create Location")
 
+@login_check_decorator
 def user_handler(response, username):
     response.write("Profile {}".format(username))
 
@@ -40,10 +47,14 @@ def login_authentication(response):
     username = response.get_field('username')
     password = response.get_field('password')
     if username == 'james' and password == 'curran':
-        response.write('Logged in successfully')
         response.set_secure_cookie('username', username)
+        response.redirect("/")
     else:
         response.write('Incorrect username or password')
+
+def logout_handler(response):
+    response.clear_cookie("username")
+    response.redirect("/")
 
 if __name__ == '__main__':
     server = Server()
@@ -55,4 +66,5 @@ if __name__ == '__main__':
     server.register("/location/create", create_handler)
     server.register("/account/profile/([a-z0-9A-Z._]+)", user_handler)
     server.register("/account/profile", profile_handler)
+    server.register("/account/logout", logout_handler)
     server.run()
