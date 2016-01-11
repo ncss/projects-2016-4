8ecbd924c7400b8742bdc92256563ad79c5b2e8c
matts1 <mattstark75@gmail.com>
2016-01-12 10:24:01 +1100

    Matt: Added a python script so everyone can add sample data

--- db/sample.sql
+++ db/sample.sql
@@ -50,3 +50,19 @@ INSERT INTO comments(author, comment, place)
 
 INSERT INTO comments(author, comment, place)
   VALUES(4, 'Where is the green sheep?', 1);
+INSERT INTO "comments" VALUES(1,1,'Its like there are only two people here',2);
+INSERT INTO "comments" VALUES(2,4,'Where is the green sheep?',1);
+INSERT INTO "locations" VALUES(1,'SIT','School of Information Technologies','sit.jpg',1,'USyd',-33.89,151.2);
+INSERT INTO "locations" VALUES(2,'UNSW','Trash','unsw.jpg',1,'nowhere',-33.91,151.2);
+INSERT INTO "locations" VALUES(3,'The Womens College','Cool place','womens_college.jpg',2,'USyd',-33.89,151.2);
+INSERT INTO "locations" VALUES(4,'The mens College','Cool place','womens_college.jpg',5,'USyd',-33.89,151.2);
+INSERT INTO "ratings" VALUES(1,5,1);
+INSERT INTO "ratings" VALUES(2,1,1);
+INSERT INTO "tags" VALUES(1,'univeristy',1);
+INSERT INTO "tags" VALUES(2,'univeristy',2);
+INSERT INTO "tags" VALUES(3,'trash',2);
+INSERT INTO "tags" VALUES(4,'sleep',3);
+INSERT INTO "users" VALUES(1,'drjc','whereisthegreensheep',NULL,'james.r.curran@sydney.edu.au','James','Curran');
+INSERT INTO "users" VALUES(2,'impendingnicky','kneecaps@reforde_stroy',NULL,'summerschool@ncss.edu.au','Nicky','Kneecapper');
+INSERT INTO "users" VALUES(3,'tomtom','james',NULL,'thomas.m.curran@sscn.edu.au','Thomas','Curran');
+INSERT INTO "users" VALUES(4,'test','tset',NULL,'test@test.test','Mr','Test');
