dde64cfd3a39cc37dc7318ac6e106df09959efcc
matts1 <mattstark75@gmail.com>
2016-01-12 10:12:52 +1100

    Matt: Added a way to add your data you add in to the sample.sql

---
+++ db/add_to_sample.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+con = sqlite3.connect('database.db')
+oldfile = open('sample.sql')
+old = oldfile.readlines()
+oldfile.close()
+
+for line in con.iterdump():
+    if line.startswith('INSERT') and line not in old:
+        old.append(line + '\n')
+print(''.join(old))
